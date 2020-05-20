7018211ca88843a07b441c00f418cf755df8eea8
Hyejung Bae <waterfall49@naver.com>
2020-05-20 15:00:19 -0300

    comment

--- Selenium/superstore.py
+++ Selenium/superstore.py
@@ -1,8 +1,24 @@
 from urllib.parse import quote_plus
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import requests
 
 baseUrl = 'https://www.atlanticsuperstore.ca/search?search-bar='
 plusUrl = input('What do you want to search? : ')
 url = baseUrl + plusUrl
-print(url)
+webpage_res = requests.get(url)
+webpage = webpage_res.content
+
+# driver = webdriver.Chrome()
+# driver.get(url)
+
+# html = driver.page_source
+# soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(webpage, 'html.parser')
+
+# r = soup.select('.product-tile.product-tile--add-to-list-button.product-tile--marketplace')
+r = soup.select('product-name__item')
+print(r)
+# for i in r:
+#     print(i.select_one('product-name__item.product-name__item--name').text)
+
